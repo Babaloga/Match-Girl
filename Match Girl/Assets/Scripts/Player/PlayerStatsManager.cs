8a90b318edc07678f0ff999d4e3214932a94a11f
George <31783107+Babaloga@users.noreply.github.com>
2018-02-27 18:56:45 -0700

    Added starting variables
    added starting variables to playerstatsmanager

--- Match Girl/Assets/Scripts/Player/PlayerStatsManager.cs
+++ Match Girl/Assets/Scripts/Player/PlayerStatsManager.cs
@@ -9,6 +9,10 @@ public class PlayerStatsManager : MonoBehaviour {
     public static float hunger = 100;
     private static float _warmth;
 
+    public int startingMatches = 0;
+    public int startingMoney = 0;
+    public float startingHunger = 100;
+
     public static float Warmth
     {
         get
@@ -26,6 +30,10 @@ public class PlayerStatsManager : MonoBehaviour {
 
     void Start () {
         temperatureClass = FindObjectOfType<PlayerTemperature>();
+
+        matches = startingMatches;
+        money = startingMoney;
+        hunger = startingHunger;
 	}
 
     private void Update()
