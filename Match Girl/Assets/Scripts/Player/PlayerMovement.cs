62591b0033ad990c30f65f136b4b508b1d4329bd
George <31783107+Babaloga@users.noreply.github.com>
2018-03-08 21:58:25 -0700

    Vertical Movement
    Added normal based movement to allow player to climb slopes.

--- Match Girl/Assets/Scripts/Player/PlayerMovement.cs
+++ Match Girl/Assets/Scripts/Player/PlayerMovement.cs
@@ -12,20 +12,24 @@ public class PlayerMovement : MonoBehaviour {
 
     public static GameObject player;
 
+    public LayerMask groundMask;
+
     private bool frozen = false;
+
+    private Vector3 normal = Vector3.up;
 
 	void Start () {
         player = gameObject;
         speed = baseSpeed;
 	}
-	
-	void FixedUpdate () {
 
+    private void FixedUpdate()
+    {
         if (!frozen)
         {
-            movement = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+            movement = Quaternion.FromToRotation(Vector3.up, normal) * Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
 
-            transform.Translate(movement * (speed * Time.fixedDeltaTime));
+            transform.Translate(movement * (speed * Time.deltaTime));
         }
     }
 
@@ -43,11 +47,20 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             speed = (1f / 2f) * baseSpeed;
-            print("Speed: "+speed);
         }
         else
         {
             speed = baseSpeed;
+        }
+
+        Collider coll = GetComponent<Collider>();
+
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(new Ray(transform.position, Vector3.down), out hit, Mathf.Infinity, groundMask))
+        {
+            normal = hit.normal;
+
+            Debug.DrawLine(transform.position, hit.point);
         }
     }
 }
