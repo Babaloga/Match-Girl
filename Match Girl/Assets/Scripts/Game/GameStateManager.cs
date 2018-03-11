fe9dfcbc482d5a277f78bb0d18877bc4bbe9708e
George <31783107+Babaloga@users.noreply.github.com>
2018-03-10 21:42:15 -0700

    Game State Manager

---
+++ Match Girl/Assets/Scripts/Game/GameStateManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+
+	public enum GameState
+    {
+        playing,
+        dialogue,
+        menu,
+        scripted
+    }
+
+    public static GameState state;
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case GameState.playing:
+
+                //Normal HUD UI
+                //Player has control
+
+                break;
+
+            case GameState.dialogue:
+
+                //Dialogue UI
+                //Player is frozen
+
+                break;
+
+            case GameState.menu:
+
+                //Menu UI
+                //Player is frozen
+                //Game is frozen
+
+                break;
+
+            case GameState.scripted:
+
+                //No UI
+                //Objects controlled by script
+
+                break;
+        }
+    }
+}
