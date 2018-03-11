20878b5eec885d7b4a6ac72e38814736c4188a4f
George <31783107+Babaloga@users.noreply.github.com>
2018-03-11 01:34:38 -0700

    NPC random appearence

---
+++ Match Girl/Assets/Scripts/NPCs/Generation/Archetype.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Archetype : ScriptableObject {
+
+    public Division[] hats;
+    public Division[] heads;
+    public Division[] bodies;
+    public Division[] legs;
+    public Division[] feet;
+
+}
---
+++ Match Girl/Assets/Scripts/NPCs/Generation/Generator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Anima2D;
+
+public class Generator : MonoBehaviour {
+
+    public Archetype archetype;
+
+    public SpriteMeshInstance head;
+    public SpriteMeshInstance torso;
+    public SpriteMeshInstance hat;
+    public SpriteMeshInstance rightThigh;
+    public SpriteMeshInstance leftThigh;
+    public SpriteMeshInstance rightShin;
+    public SpriteMeshInstance rightUpperArm;
+    public SpriteMeshInstance leftShin;
+    public SpriteMeshInstance leftFoot;
+    public SpriteMeshInstance leftForearm;
+    public SpriteMeshInstance leftUpperArm;
+    public SpriteMeshInstance leftHand;
+    public SpriteMeshInstance rightFoot;
+    public SpriteMeshInstance rightHand;
+    public SpriteMeshInstance rightForearm;
+
+    private void Start()
+    {
+        GenerateNPC();
+    }
+
+    public void GenerateNPC()
+    {
+        Division d_hat = archetype.hats[Random.Range(0, archetype.hats.Length)];
+        hat.spriteMesh = d_hat.parts[0].spriteMesh;
+
+        Division d_head = archetype.heads[Random.Range(0, archetype.heads.Length)];
+        head.spriteMesh = d_head.parts[0].spriteMesh;
+        rightHand.spriteMesh = d_head.parts[2].spriteMesh;
+        leftHand.spriteMesh = d_head.parts[1].spriteMesh;
+
+        Division d_body = archetype.bodies[Random.Range(0, archetype.bodies.Length)];
+        torso.spriteMesh = d_body.parts[0].spriteMesh;
+        rightUpperArm.spriteMesh = d_body.parts[1].spriteMesh;
+        rightForearm.spriteMesh = d_body.parts[2].spriteMesh;
+        leftUpperArm.spriteMesh = d_body.parts[3].spriteMesh;
+        leftForearm.spriteMesh = d_body.parts[4].spriteMesh;
+
+        Division d_legs = archetype.legs[Random.Range(0, archetype.legs.Length)];
+        rightThigh.spriteMesh = d_legs.parts[0].spriteMesh;
+        rightShin.spriteMesh = d_legs.parts[1].spriteMesh;
+        leftThigh.spriteMesh = d_legs.parts[2].spriteMesh;
+        leftShin.spriteMesh = d_legs.parts[3].spriteMesh;
+
+        Division d_feet = archetype.feet[Random.Range(0, archetype.feet.Length)];
+        leftFoot.spriteMesh = d_feet.parts[0].spriteMesh;
+        rightFoot.spriteMesh = d_feet.parts[1].spriteMesh;
+    }
+}
