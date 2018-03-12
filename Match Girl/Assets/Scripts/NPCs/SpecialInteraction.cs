34222db5eede18da25326150b4984accf267a10b
George <31783107+Babaloga@users.noreply.github.com>
2018-03-12 13:30:01 -0600

    Added special interaction script
    Added a script for special interactions and a spacebar tooltip, and organized the sprites folder a little bit.

---
+++ Match Girl/Assets/Scripts/NPCs/SpecialInteraction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpecialInteraction : MonoBehaviour {
+
+    public float radius = 5f;
+
+    public Dialogue dialogue;
+
+    public Canvas tooltip;
+
+    public bool oneTimeInteraction = false;
+
+	void Start () {
+        if(!tooltip) tooltip = GetComponentInChildren<Canvas>();
+	}
+}
