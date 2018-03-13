b7c7da8c3e95f279775a468de4d80fdc5c7e7715
George <31783107+Babaloga@users.noreply.github.com>
2018-03-12 18:37:30 -0600

    Automatic Day End plus end text and fader
    Added code to automatically load the end of day screen, as well as an end message and a fader script to smooth the transition.

---
+++ Match Girl/Assets/Scripts/Game/Fader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fader : MonoBehaviour {
+
+    public CanvasGroup fader;
+
+    public bool fadeOnAwake = true;
+
+    public float fadeDuration = 3f;
+
+    private bool fadingOut = false;
+    private bool fadingIn = false;
+
+    private void Awake()
+    {
+        fader = GetComponent<CanvasGroup>();
+        if (fadeOnAwake)
+        {
+            fader.alpha = 1;
+            fadingOut = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (fadingOut && fader.alpha > 0)
+        {
+            fader.alpha -= (1f / fadeDuration) * Time.deltaTime;
+        }
+        else if (fadingIn && fader.alpha < 1)
+        {
+            fader.alpha += (1f / fadeDuration) * Time.deltaTime;
+        }
+    }
+
+    public void FadeIn()
+    {
+        fadingIn = true;
+        fadingOut = false;
+    }
+
+    public void FadeOut()
+    {
+        fadingIn = false;
+        fadingOut = true;
+    }
+
+}
