5064e0b48acc653871f2acfb755bd99d6a221970
George <31783107+Babaloga@users.noreply.github.com>
2018-03-26 16:47:12 -0600

    Better UI setup
    started implimenting the improved UI

---
+++ Match Girl/Assets/Scripts/UI/RelativePositioner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelativePositioner : MonoBehaviour {
+
+    public Vector2 distance = Vector2.zero;
+
+    public RectTransform target;
+
+    RectTransform thisRect;
+
+    private void Start()
+    {
+        thisRect = transform as RectTransform;
+    }
+
+    private void Update()
+    {
+        thisRect.position = target.position + (Vector3)(target.rect.center - new Vector2(0, (target.rect.height / 2) * target.lossyScale.y)) + (Vector3) distance;
+    }
+}
