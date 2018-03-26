b7438e85545a8f392c2e598474dddf690c679a54
George <31783107+Babaloga@users.noreply.github.com>
2018-03-26 14:42:56 -0600

    Fixed Building Fade
    allowed the buildingCamPass script to work with meshes with multiple materials

--- Match Girl/Assets/Scripts/Camera/BuildingCamPass.cs
+++ Match Girl/Assets/Scripts/Camera/BuildingCamPass.cs
@@ -10,12 +10,12 @@ public class BuildingCamPass : MonoBehaviour {
 
     public Transform target;
     public Collider camBubble;
-    Collider building;
+    public Collider building;
 
     public float fadeRate = 1.5f;
 
-    public Material opaqueMaterial;
-    Material instanceMaterial;
+    public Material[] opaqueMaterials;
+    public Material[] instanceMaterials;
 
     float time1 = 0;
     float time2 = 0;
@@ -27,10 +27,22 @@ public class BuildingCamPass : MonoBehaviour {
 
     private void Start()
     {
-        building = GetComponent<Collider>();
+        if(!building) building = GetComponent<Collider>();
         if(!rend) rend = GetComponent<MeshRenderer>();
-        instanceMaterial = new Material(rend.material);
-        rend.material = instanceMaterial;
+        opaqueMaterials = rend.materials;
+        List<Material> startMaterialsTemp = new List<Material>();
+        for (int i = 0; i < opaqueMaterials.Length; i++)
+        {
+            Material m = new Material(opaqueMaterials[i]);
+
+            m.name += "[INSTANCE]";
+
+            StandardShaderUtils.ChangeRenderMode(m, StandardShaderUtils.BlendMode.Fade);
+
+            startMaterialsTemp.Add(m);
+            //print(i + " " + string.Join(",", instanceMaterials[i].shaderKeywords));
+        }
+        instanceMaterials = startMaterialsTemp.ToArray();
     }
 
     private void Update()
@@ -71,14 +83,27 @@ public class BuildingCamPass : MonoBehaviour {
         if(behind && bubbleOverlaps)
         {
             time1 = Time.time;
-            rend.material = instanceMaterial;
-            rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, Mathf.Lerp(1, 0, (Time.time - time2) / fadeRate));
+            print("behind " + opaqueMaterials.Length);
+            Material[] tempMaterials = rend.materials;
+            for (int i = 0; i < opaqueMaterials.Length; i++)
+            {
+                tempMaterials[i] = instanceMaterials[i];
+                tempMaterials[i].color = new Color(tempMaterials[i].color.r, tempMaterials[i].color.g, tempMaterials[i].color.b, Mathf.Lerp(1, 0, (Time.time - time2) / fadeRate));
+            }
+            rend.materials = tempMaterials;
+
         }
         else
         {
             time2 = Time.time;
-            rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, Mathf.Lerp(0, 1, (Time.time - time1) / fadeRate));
-            if(rend.material.color.a >= 1) rend.material = opaqueMaterial;
+            print("not behind " + opaqueMaterials.Length);
+            Material[] tempMaterials = rend.materials;
+            for (int i = 0; i < opaqueMaterials.Length; i++)
+            {
+                tempMaterials[i].color = new Color(tempMaterials[i].color.r, tempMaterials[i].color.g, tempMaterials[i].color.b, Mathf.Lerp(0, 1, (Time.time - time1) / fadeRate));
+                if (rend.material.color.a >= 1) tempMaterials[i] = opaqueMaterials[i];
+            }
+            rend.materials = tempMaterials;
         }
     }
 
@@ -86,4 +111,59 @@ public class BuildingCamPass : MonoBehaviour {
     //{
     //    Gizmos.DrawCube(building.bounds.center, building.bounds.size);
     //}
+}
+
+ public static class StandardShaderUtils
+{
+    public enum BlendMode
+    {
+        Opaque,
+        Cutout,
+        Fade,
+        Transparent
+    }
+
+    public static void ChangeRenderMode(Material standardShaderMaterial, BlendMode blendMode)
+    {
+        switch (blendMode)
+        {
+            case BlendMode.Opaque:
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                standardShaderMaterial.SetInt("_ZWrite", 1);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = -1;
+                break;
+            case BlendMode.Cutout:
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                standardShaderMaterial.SetInt("_ZWrite", 1);
+                standardShaderMaterial.EnableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = 2450;
+                break;
+            case BlendMode.Fade:
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                standardShaderMaterial.SetInt("_ZWrite", 0);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.EnableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = 3000;
+                break;
+            case BlendMode.Transparent:
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                standardShaderMaterial.SetInt("_ZWrite", 0);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = 3000;
+                break;
+        }
+
+    }
 }
