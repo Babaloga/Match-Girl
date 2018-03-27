7314cb8f7ba870d1dde63017dbb25017aac68545
George <31783107+Babaloga@users.noreply.github.com>
2018-03-26 20:09:01 -0600

    Added code for three stage frost UI

---
+++ Match Girl/Assets/Scripts/UI/TemperatureUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureUI : MonoBehaviour {
+
+    public CanvasGroup frostOne;
+    public CanvasGroup frostTwo;
+    public CanvasGroup frostThree;
+
+    public AnimationCurve frostOneCurve;
+    public AnimationCurve frostTwoCurve;
+    public AnimationCurve frostThreeCurve;
+
+	void Update () {
+        float t = PlayerStatsManager.Warmth;
+
+        frostOne.alpha = frostOneCurve.Evaluate(t/100f);
+        frostTwo.alpha = frostTwoCurve.Evaluate(t/100f);
+        frostThree.alpha = frostThreeCurve.Evaluate(t/100f);
+    }
+}
