3aac22c5aa1d2b280fc2326a26be2e35524163b9
George <31783107+Babaloga@users.noreply.github.com>
2018-04-04 21:16:10 -0600

    Dialogue Reaction
    Integrated current dialogue system into the interaction system.

---
+++ Match Girl/Assets/Scripts/ScriptableObjects/Interaction/Reactions/ImmediateReactions/DialogueReaction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueReaction : Reaction {
+
+
+    private DialogueReader reader;
+
+    protected override void SpecificInit()
+    {
+        reader = DialogueReader.reader;
+    }
+
+
+    protected override void ImmediateReaction()
+    {
+        reader.StartDialogue(dialogue);
+    }
+}
