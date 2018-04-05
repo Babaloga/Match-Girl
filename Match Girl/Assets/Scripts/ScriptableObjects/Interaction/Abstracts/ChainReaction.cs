57f00d4219f3e16a81266949977055721b79bd27
George <31783107+Babaloga@users.noreply.github.com>
2018-04-04 20:48:08 -0600

    Conditions and Reactions
    Ported over the interactions scripts from last semester.

---
+++ Match Girl/Assets/Scripts/ScriptableObjects/Interaction/Abstracts/ChainReaction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public abstract class ChainReaction : Reaction
+{
+    public float delay = 0;
+
+    public ReactionCollection[] reactArray;
+
+    protected abstract bool ProceedCondition();
+
+    protected abstract int ReactCondition();
+
+    public new void React(MonoBehaviour monoBehaviour)
+    {
+        monoBehaviour.StartCoroutine(ReactCoroutine());
+    }
+
+    protected override void ImmediateReaction() { }
+
+    protected IEnumerator ReactCoroutine()
+    {
+        yield return new WaitForSeconds(delay);
+
+        ImmediateReaction();
+
+        yield return new WaitUntil(() => ProceedCondition() == true);
+
+        if (reactArray[ReactCondition()])
+        {
+            reactArray[ReactCondition()].React();
+        }
+    }
+
+}
