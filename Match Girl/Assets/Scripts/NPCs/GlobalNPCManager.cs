b73ce911a380de74528e1d2b02f22bb8f39d8dc5
George <31783107+Babaloga@users.noreply.github.com>
2018-04-11 15:29:48 -0600

    NPC Manager
    Added global NPC manager to spawn new NPCs when populations get too low.

---
+++ Match Girl/Assets/Scripts/NPCs/GlobalNPCManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalNPCManager : MonoBehaviour {
+
+    public int minimumNPCCount = 50;
+
+    int currentNPCCount = 0;
+
+    public GameObject maleNPCPrefab;
+    public GameObject femaleNPCPrefab;
+
+    private void Start()
+    {
+        UpdateNPCCount();
+    }
+
+    private void Update()
+    {
+        if (Time.frameCount % 60 == 0) UpdateNPCCount();
+    }
+
+    void UpdateNPCCount()
+    {
+        currentNPCCount = FindObjectsOfType<MoveTo>().Length;
+
+        print(currentNPCCount);
+
+        if(currentNPCCount < minimumNPCCount)
+        {
+            StartCoroutine(SpawnNPCs(minimumNPCCount - currentNPCCount));
+        }
+    }
+
+    IEnumerator SpawnNPCs (int count)
+    {
+        print(count);
+
+        for (int i = 0; i < count; i++)
+        {
+            if (Random.Range(0, 2) > 0) Instantiate(maleNPCPrefab);
+            else Instantiate(femaleNPCPrefab);
+
+            yield return null;
+        }
+    }
+
+}
