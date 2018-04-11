8c00c050662d841dc8b2eee8c7ca53fb73da8720
George <31783107+Babaloga@users.noreply.github.com>
2018-04-10 19:34:13 -0600

    Dialogue Organizing and Continue To option

---
+++ Match Girl/Assets/Scripts/World/MoveAndTile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAndTile : MonoBehaviour {
+
+    public float distance = 10f;
+
+    Vector3 startPos;
+
+    public Vector3 movement = Vector3.zero;
+
+    bool spawnedNext = false;
+
+    public GameObject partner;
+
+    void Awake () {
+        startPos = transform.position;
+	}
+	
+	void FixedUpdate () {
+        transform.position += movement * Time.fixedDeltaTime;
+
+        if((transform.position - startPos).magnitude >= distance && !partner)
+        {
+            partner = Instantiate(gameObject, startPos, Quaternion.Euler(Vector3.zero));
+            partner.GetComponent<MoveAndTile>().partner = gameObject;
+        }
+
+        if ((transform.position - startPos).magnitude >= 2f * distance)
+        {
+            transform.position = startPos;
+        }
+    }
+}
