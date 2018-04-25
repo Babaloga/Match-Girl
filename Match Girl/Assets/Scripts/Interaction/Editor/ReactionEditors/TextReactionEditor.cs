4d2f82cfcbfd4ccbf73e2d98ec22f45c19bb8c73
George <31783107+Babaloga@users.noreply.github.com>
2018-04-24 18:00:56 -0600

    Dialogue and text reaction fix

---
+++ Match Girl/Assets/Scripts/Interaction/Editor/ReactionEditors/TextReactionEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TextReaction))]
+public class TextReactionEditor : ReactionEditor
+{
+    protected override string GetFoldoutLabel()
+    {
+        return "Text Reaction";
+    }
+}
+
